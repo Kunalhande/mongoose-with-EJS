b681e2332b4aa8850947d779ab2c26149c7861da
kunal hande <kunalhand30@gmail.com>
2025-06-19 10:23:00 +0530

    fourth commit

---
+++ .history/init_20250619101447.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+
+main().then(() =>{
+     console.log("connection successful")
+})
+.catch((err) => {console.log(err)});
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let chats = [
+    {
+        from:"kunal",
+        To:"rushi",
+        message:"hi, how are you",
+        created_at:new Date,
+    },
+{
+    from:"ram",
+        To:"sham",
+        message:"hi, what are you",
+        created_at:new Date,
+},
+{
+    from:"rohit",
+        To:"aadi",
+        message:"hi, where are you",
+        created_at:new Date,
+}]
+
+chat.insertMany(chats)
---
+++ .history/init_20250619101958.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const chat = require("./models/chat");
+
+
+main().then(() =>{
+     console.log("connection successful")
+})
+.catch((err) => {console.log(err)});
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let chats = [
+    {
+        from:"kunal",
+        To:"rushi",
+        message:"hi, how are you",
+        created_at:new Date,
+    },
+{
+    from:"ram",
+    To:"sham",
+    message:"hi, what are you",
+    created_at:new Date,
+},
+{
+    from:"rohit",
+    To:"aadi",
+     message:"hi, where are you",
+     created_at:new Date,
+}]
+
+chat.insertMany(chats);
